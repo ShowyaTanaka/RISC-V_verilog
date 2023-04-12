847cc71db6ec9114d56ab54c0def035c9ddfd368
ShowyaTanaka <shoyatanakatst@gmail.com>
2023-04-12 14:31:18 +0900

    alu修正

--- alu32.v
+++ alu32.v
@@ -11,11 +11,13 @@ ALUオペコード
 仕様メモ
 ビットシフトする場合in1に数字が入ることを期待しない。仮に数字が入力されていたとしても一切無視する。
 */
-module ALU32(in0,in1,op,out);
+module ALU32(in0,in1,op,out,of_detect);
     input[31:0] in0;
     input[31:0] in1;
     input[2:0] op;
-    output reg[31:0] out;
+    output reg[30:0] out;
+    output reg of_detect;
+    reg [31:0] val;
     always @* begin
         if (op==0) begin
             out <= in0&in1;
@@ -23,10 +25,16 @@ module ALU32(in0,in1,op,out);
          else if (op == 1) begin
             out <= in0|in1;
         end else if (op == 2) begin
-            out <= in0 + in1;
+            //ほぼ必要ないと思うけどオーバーフロー一応検知しておきます。
+            val <= in0 + in1;
+            out <= val[30:0];
+            of_detect <= val[31:31];
         end else if (op == 3 | op == 4) begin
-            //最初の加算機のみはin1をビット反転させて1加える処理が必要。
-            out <= in0 - in1;
+            val <= in0 - in1;
+            $displayb(val);
+            $displayb(val);
+            out <= val[30:0];
+            of_detect <= val[31:31];
         end
         else if (op == 5) begin
             out <= in0 ~| in1;
@@ -44,14 +52,16 @@ module test();
     reg [31:0] a;
     reg [31:0] b;
     reg [2:0] op;
-    output [31:0] out;
-    ALU32 alu(.in0(a),.in1(b),.op(op),.out(out));
+    output [30:0] out;
+    output of_detect;
+    ALU32 alu(.in0(a),.in1(b),.op(op),.out(out),.of_detect(of_detect));
     initial begin
         #0
-        a = 4'b0001;
-        b = 4'b0001;
+        a = 2147483647;
+        b = 2147483648;
         op = 4'b0011;
         #10
         $displayb(out);
+        $displayb(of_detect);
     end
 endmodule
